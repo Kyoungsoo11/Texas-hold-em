c907736d7bed8426f527d79291329aa4d651e14e
Kyoungsoo11 <105273785+Kyoungsoo11@users.noreply.github.com>
2022-07-03 18:40:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ enum number {
 } NUMBER;
 void print_deck(char d[4][13])
 {
-	int x = 70, y = 1;//Locate
+	int x = 100, y = 5;//Locate
 	int i, j;
 	for (i = 0; i < 4; i++)
 	{
@@ -183,25 +183,114 @@ void get_card(int* t, int* n)
 		*n = King;
 		break;
 	}
-	printf("%d %d\n", *t, *n);
-	printf("\n");
-	
 }
-void calculate(struct Card* s)
+int calculate(struct Card* com, struct Card* hand)
 {
-	int i;
-	//Royal Straigt Flush
-	for (i = 0; i < 7; i++)
+	struct Card s[COMSIZE + HANDSIZE];
+	int i, j, k;
+	char t, n;
+	char Scount, Smem[COMSIZE + HANDSIZE], Dcount, Dmem[COMSIZE + HANDSIZE], Hcount, Hmem[COMSIZE + HANDSIZE], Ccount, Cmem[COMSIZE + HANDSIZE];
+	//Compound hand and community
+	for (i = 0; i < COMSIZE; i++)
 	{
-
+		s[i] = *(com + i);
 	}
-	//Straigt Flush
+	for (j = 0; j < HANDSIZE; j++)
+	{
+		s[i + j] = *(hand + j);
+	}
+	//print s
+	for (k = 0; k < COMSIZE + HANDSIZE; k++)
+	{
+		i = s[k].type;
+		j = s[k].number;
+		switch (i)
+		{
+		case 0:
+			Color(WHITE, BLACK);
+			printf("♠");
+			break;
+		case 1:
+			Color(WHITE, RED);
+			printf("◆");
+			break;
+		case 2:
+			Color(WHITE, RED);
+			printf("♥");
+			break;
+		case 3:
+			Color(WHITE, BLACK);
+			printf("♣");
+			break;
+		}
+		switch (j)
+		{
+		case 0://A
+			printf("A");
+			break;
+		case 1://2
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			printf("%d", j + 1);
+			break;
+		case 9://10
+			printf("T");
+			break;
+		case 10://J
+			printf("J");
+			break;
+		case 11://Q
+			printf("Q");
+			break;
+		case 12://K
+			printf("K");
+			break;
+		}
+		Color(BLACK, WHITE); printf(" ");
+	}
+	//Flush
+	Scount = Dcount = Hcount = Ccount = 0;
+	for (i = 0; i < COMSIZE + HANDSIZE; i++)
+	{
+		t = s[i].type;
+		switch (t)
+		{
+		case spade:
+			Smem[Scount] = i;
+			Scount++;
+			break;
+		case diamond:
+			Dmem[Dcount] = i;
+			Dcount++;
+			break;
+		case heart:
+			Hmem[Hcount] = i;
+			Hcount++;
+			break;
+		case clover:
+			Cmem[Ccount] = i;
+			Ccount++;
+			break;
+		}
+	}
+	if (Scount >= 5 || Dcount >= 5 || Hcount >= 5 || Ccount >= 5)
+	{
+		printf("flush!!");
+	}
+	return 0;
 }
 int main()
 {
+	system("mode con cols=200 lines=50");
 	int in_t, in_n, t, n;
 	int path, player_num;
-	struct Card community[5], My_Hand[2];//
+	time_t t1, t2;//To get processing time
+	struct Card community[COMSIZE], My_Hand[HANDSIZE];//
 	char deck[4][13] = { {0}, };//[type][number]
 
 player_number://get player num
@@ -232,7 +321,43 @@ M2:
 	My_Hand[1].type = in_t;
 	My_Hand[1].number = in_n;
 	deck[in_t][in_n] = 1;
+C1:
+	printf("Enter your community\n");
+	get_card(&in_t, &in_n);
+	community[0].type = in_t;
+	community[0].number = in_n;
+	deck[in_t][in_n] = 1;
+C2:
+	printf("Enter your community\n");
+	get_card(&in_t, &in_n);
+	community[1].type = in_t;
+	community[1].number = in_n;
+	deck[in_t][in_n] = 1;
+C3:
+	printf("Enter your community\n");
+	get_card(&in_t, &in_n);
+	community[2].type = in_t;
+	community[2].number = in_n;
+	deck[in_t][in_n] = 1;
+C4:
+	printf("Enter your community\n");
+	get_card(&in_t, &in_n);
+	community[3].type = in_t;
+	community[3].number = in_n;
+	deck[in_t][in_n] = 1;
+C5:
+	printf("Enter your community\n");
+	get_card(&in_t, &in_n);
+	community[4].type = in_t;
+	community[4].number = in_n;
+	deck[in_t][in_n] = 1;
 
+	t1 = time(NULL);
+	calculate(community, My_Hand);
+	t2 = time(NULL);
+	printf("%lld", t2 - t1);//Get processing time;
 	print_deck(deck);
+
+	gotoxy(0, 48);
 	return 0;
 }
